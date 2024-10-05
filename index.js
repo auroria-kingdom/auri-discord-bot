dc38e5dca9209d12591f9f51a42f22bf588e9868
Huto <92351427+Hutotpn@users.noreply.github.com>
2024-10-05 07:30:37 +0000

    gitignore

--- index.js
+++ index.js
@@ -56,12 +56,12 @@ client.on(Events.InteractionCreate, async (interaction) => {
     console.error(error);
     if (interaction.replied || interaction.deferred) {
       await interaction.followUp({
-        content: "There was an error while executing this command!",
+        content: "There was an error while executing this command.",
         ephemeral: true,
       });
     } else {
       await interaction.reply({
-        content: "There was an error while executing this command!",
+        content: "There was an error while executing this command.",
         ephemeral: true,
       });
     }
